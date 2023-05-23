1d556a8803d7a9a79051f1f0e4590d4800c22bb7
Eguchi <eguchi@eguchi.cc>
2023-05-23 17:43:09 +0900

    tailwindcssのインストール

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function IndexPage() {
-  return <div>index page</div>;
+  return (
+    <div>
+      <p className="text-teal-500 font-bold text-center text-4xl tracking-wider">
+        index page
+      </p>
+    </div>
+  );
 }
